fa25c685c5c8d666ed7a9555a318ad533282188e
Martin Wang <wangxuesong@gmail.com>
2021-02-13 15:03:47 +0800

    add accept for Acceptor

--- src/server.rs
+++ src/server.rs
@@ -22,7 +22,7 @@ impl Paxos for PaxosService {
         {
             let mut storage = self.storage.lock().unwrap();
             if storage.contains_key(&key) {
-                let mut value = storage.get(&key).cloned().unwrap();
+                let value = storage.get(&key).cloned().unwrap();
                 if request_round.number > value.round.as_ref().unwrap().number {
                     // 保存请求中的 round 到 last_round
                     let new_value = Acceptor {
@@ -48,7 +48,25 @@ impl Paxos for PaxosService {
     }
 
     async fn accept(&self, request: Request<Proposer>) -> Result<Response<Acceptor>, Status> {
-        unimplemented!()
+        let proposer = request.get_ref();
+        let id = proposer.id.clone().unwrap();
+        let key = id.key;
+        let request_round = proposer.round.as_ref().unwrap();
+        let request_value = proposer.value.clone().unwrap();
+
+        // for lock storage
+        {
+            let mut storage = self.storage.lock().unwrap();
+            let acc = storage.get(&key).cloned().unwrap();
+            if request_round.number >= acc.last_round.clone().unwrap().number {
+                let mut new_value = acc.clone();
+                new_value.round = Some(request_round.clone());
+                new_value.value = Some(request_value);
+                new_value.last_round = Some(request_round.clone());
+                storage.insert(key, new_value);
+            }
+            Ok(Response::new(acc))
+        } // unlock storage
     }
 }
 
@@ -58,7 +76,7 @@ mod tests {
     use tokio::runtime::Runtime;
 
     #[test]
-    fn test_prepare_phase_1() {
+    fn test_prepare() {
         let service = PaxosService {
             storage: Default::default(),
         };
@@ -118,6 +136,61 @@ mod tests {
                 }),
                 value: None,
             }
+        );
+    }
+
+    #[test]
+    fn test_accept() {
+        let service = PaxosService {
+            storage: Default::default(),
+        };
+        let proposer = Proposer {
+            id: Some(PaxosInstanceId {
+                key: "test1".to_string(),
+                version: 0,
+            }),
+            round: Some(RoundNum {
+                number: 1,
+                proposer_id: 0,
+            }),
+            value: Some(Value { value: 11 }),
+        };
+        let r0 = Request::new(proposer.clone());
+        let r = Runtime::new().unwrap().block_on(service.prepare(r0));
+        assert!(r.is_ok());
+        let resp = r.unwrap();
+        let acc = resp.get_ref();
+        assert_eq!(
+            &Acceptor {
+                round: Some(RoundNum::default()),
+                last_round: Some(RoundNum {
+                    number: 0,
+                    proposer_id: 0,
+                }),
+                value: None,
+            },
+            acc
+        );
+
+        let r = Runtime::new()
+            .unwrap()
+            .block_on(service.accept(Request::new(proposer.clone())));
+        assert!(r.is_ok());
+        let resp = r.unwrap();
+        let acc = resp.get_ref();
+        assert_eq!(
+            &Acceptor {
+                round: Some(RoundNum {
+                    number: 0,
+                    proposer_id: 0
+                }),
+                last_round: Some(RoundNum {
+                    number: 1,
+                    proposer_id: 0,
+                }),
+                value: None,
+            },
+            acc
         );
     }
 }
